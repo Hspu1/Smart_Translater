6a30a47264630c0bd2726506f3f21f02c84cee23
Макс <rumyantsev1m0s1@gmail.com>
2025-07-01 00:20:09 +0300

    Сделан некий каркас проекта

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi.responses import ORJSONResponse
+from uvicorn import run
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Smart Translater",
+    default_response_class=ORJSONResponse
+)
+
+
+if __name__ == '__main__':
+    run(
+        app="app.main:app", port=8000, host="127.0.0.1",
+        reload=False, use_colors=True
+    )
